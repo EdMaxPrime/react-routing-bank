4ba732610062fc61489ea78c18dda06927906cc6
Xing Tao Shi <xshi@stuy.edu>
2020-01-11 01:18:34 -0500

    added color being red

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 
 class AccountBalance extends Component {
   render() {
-    return (
-        <div>
-          Balance: {this.props.accountBalance}
-        </div>
-    );
+      if (this.props.accountBalance > 0) {
+	  return (
+		  <div style={{color:"blue"}}>
+		  Balance: {this.props.accountBalance}
+		  </div>	  
+		  );
+      }
+      else {
+	  return (
+		  <div style={{color:"red"}}>
+		  Balance: {this.props.accountBalance}
+		  </div>	  
+		  );
+      }
   }
 }
 
