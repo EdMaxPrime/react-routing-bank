39a295fa1c1527677b670e5edf3226076282b0ad
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-11 04:19:22 -0500

    styled the account balance

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -1,22 +1,21 @@
 import React, {Component} from 'react';
+import "./common.css";
 
 class AccountBalance extends Component {
-  render() {
-      if (this.props.accountBalance > 0) {
-	      return (
-		      <div style={{color:"blue"}}>
-		        Balance: ${this.props.accountBalance.toFixed(2)}
-		      </div>	  
-		    );
-      }
-      else {
-	      return (
-		      <div style={{color:"red"}}>
-		        Balance: ${this.props.accountBalance.toFixed(2)}
-		      </div>	  
-		    );
-      }
-  }
+    render() {
+  	    var color;
+        if (this.props.accountBalance > 0) {
+        	color = "blue";
+        } else {
+        	color = "red";
+        }
+	    return (
+		    <div className="card">
+		      	<h4 className="card-title">Balance</h4>
+		      	<div style={{color: color}}>${this.props.accountBalance.toFixed(2)}</div>
+		    </div>	  
+		);
+    }
 }
 
 export default AccountBalance;
